efc56a8e34674c41c208bb1337c183fef3ba8398
vinamravinay12 <vinamra.vinay235@gmail.com>
2016-08-28 03:23:16 -0700

    Merge branch 'master' of C:\Users\vinam\Documents\Neighborly\Neighborly with conflicts.

--- backend/main.js
+++ backend/main.js
@@ -1,2 +1,6 @@
-/* Hello, World! program in node.js */
-console.log("Hello, World!")
+var http = require('http');
+http.createServer(function(request,response){
+        response.writeHead(200,{'content-type':'text/plain'});
+        response.end('Hello world\n');
+}).listen(8080);
+console.log('Server running  at http://127.0.0.1:8080');
